52749ff13cb8f9a6ccb557c7067618f696bdce08
Vladimir Serov <me@cab404.ru>
2021-02-07 07:11:59 +0300

    rustfmt

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 with import <nixpkgs> {}; mkShell {
-  buildInputs = [ cargo rustc ] ++ [ pkgconfig libudev.dev libusb1.dev dbus.dev ];
+  buildInputs = [ cargo rustc rustfmt ] ++ [ pkgconfig libudev.dev libusb1.dev dbus.dev ];
   RUST_SRC_PATH = rustPlatform.rustLibSrc;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,54 @@
-use buttplug::{client::{ButtplugClient, ButtplugClientDevice, VibrateCommand}, server::ButtplugServerOptions, util::{
-        async_manager
-    }};
-use std::{sync::Arc, time::Duration};
+use buttplug::{
+    client::{ButtplugClient, ButtplugClientDevice, VibrateCommand},
+    server::ButtplugServerOptions,
+    util::async_manager,
+};
 use futures::StreamExt;
 use futures_timer::Delay;
+use std::{sync::Arc, time::Duration};
 
 fn main() {
-  println!("start...");
+    println!("start...");
 
-  async_manager::block_on(async {
-    control().await;
-  });
+    async_manager::block_on(async {
+        control().await;
+    });
 }
 
-
-async fn control () {
+async fn control() {
     println!("running control");
-    
+
     let client = ButtplugClient::new("mowmow");
     let mut events = client.event_stream();
     println!("waiting for server");
 
-    client.connect_in_process(&ButtplugServerOptions::default()).await.unwrap();
-    
+    client
+        .connect_in_process(&ButtplugServerOptions::default())
+        .await
+        .unwrap();
+
     if let Err(err) = client.start_scanning().await {
         println!("Client errored when starting scan! {}", err);
         return;
     }
 
-    let vibr = | dev: Arc<ButtplugClientDevice> | {
-        async move {
-            println!("{} should start vibrating!", dev.name);
-            loop {
-                dev.vibrate(VibrateCommand::Speed(0.9)).await.unwrap();
-                Delay::new(Duration::from_millis(500)).await;
-                dev.vibrate(VibrateCommand::Speed(0.5)).await.unwrap();
-                Delay::new(Duration::from_millis(500)).await;
-            }
+    let vibr = |dev: Arc<ButtplugClientDevice>| async move {
+        println!("{} should start vibrating!", dev.name);
+        loop {
+            dev.vibrate(VibrateCommand::Speed(0.9)).await.unwrap();
+            Delay::new(Duration::from_millis(500)).await;
+            dev.vibrate(VibrateCommand::Speed(0.5)).await.unwrap();
+            Delay::new(Duration::from_millis(500)).await;
         }
     };
 
- 
     println!("loop starting now");
-    
+
     loop {
         match events.next().await.unwrap() {
             buttplug::client::ButtplugClientEvent::ScanningFinished => {}
             buttplug::client::ButtplugClientEvent::DeviceAdded(dev) => {
-                println!("Device {devname} added!", devname = dev.name );
+                println!("Device {devname} added!", devname = dev.name);
                 vibr(dev).await;
             }
             buttplug::client::ButtplugClientEvent::DeviceRemoved(_) => {}
@@ -57,5 +58,4 @@ async fn control () {
             buttplug::client::ButtplugClientEvent::Error(_) => {}
         }
     }
-
 }
